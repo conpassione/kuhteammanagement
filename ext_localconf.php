bcd0e4e19864bbf00cdfc3c82d7297ed27dd228b
Renzo Bauen <info@conpassione.ch>
2025-09-29 22:57:39 +0200

    forms and ck-editor fixed

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,10 @@
 <?php
 declare(strict_types=1);
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+// use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die();
 
-ExtensionManagementUtility::addTypoScriptSetup(
-    '
-   module.tx_form {
-       settings {
-           yamlConfigurations {
-               36651 = EXT:kuhteammanagement/Configuration/Form/CpFormSetup.yaml
-           }
-       }
-   }
-'
-);
+//$GLOBALS['TYPO3_CONF_VARS']['MAIL']['layoutRootPaths'][36651] = 'EXT:kuhteammanagement/Resources/Private/Emails/Layouts';
+//$GLOBALS['TYPO3_CONF_VARS']['MAIL']['templateRootPaths'][36651] = 'EXT:kuhteammanagement/Resources/Private/Emails/Templates';
+//$GLOBALS['TYPO3_CONF_VARS']['MAIL']['partialRootPaths'][36651] = 'EXT:kuhteammanagement/Resources/Private/Emails/Partials';
